fa983482d2031d6875df6f491fe24a4450cfb864
Sergei Omelchenko <125047478+SerOmmm@users.noreply.github.com>
2024-01-08 10:53:43 +0300

    Add files via upload
    Задание на execute_script
    В данной задаче вам нужно будет снова преодолеть капчу для роботов и справиться с ужасным и огромным футером, который дизайнер всё никак не успевает переделать. Вам потребуется написать код, чтобы:
    1. Открыть страницу https://SunInJuly.github.io/execute_script.html.
    2. Считать значение для переменной x.
    3. Посчитать математическую функцию от x.
    4. Проскроллить страницу вниз.
    5. Ввести ответ в текстовое поле.
    6. Выбрать checkbox "I'm the robot".
    7. Переключить radiobutton "Robots rule!".
    8. Нажать на кнопку "Submit".
    Для этой задачи вам понадобится использовать метод execute_script, чтобы сделать прокрутку в область видимости элементов, перекрытых футером.

---
+++ l2.2_s6.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try: 
+    link = "https://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x = browser.find_element(By.ID, "input_value").text
+    y = calc(x)
+    browser.execute_script("window.scrollBy(0, 100);")
+    # Пишем ответ в поле
+    browser.find_element(By.ID, "answer").send_keys(y)
+    # Ставим метки
+    browser.find_element(By.ID, "robotCheckbox").click()
+    browser.find_element(By.ID, "robotsRule").click()
+    # Скроллим 
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn-primary")
+    # browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click() # давим кнопу
+finally:
+    time.sleep(10)
+    browser.quit()
